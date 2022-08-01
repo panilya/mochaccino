d0d1e5eb64f083da70280efc3ad9bed31b04b4dd
panilya <panilyau@gmail.com>
2022-08-01 18:24:06 +0300

    First commit

---
+++ mochaccino-server/src/main/java/com/panilya/mochaccinoserver/MochaccinoServerApplication.java
@@ -0,0 +1,13 @@
+package com.panilya.mochaccinoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MochaccinoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MochaccinoServerApplication.class, args);
+    }
+
+}
---
+++ mochaccino-server/src/test/java/com/panilya/mochaccinoserver/MochaccinoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panilya.mochaccinoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MochaccinoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
