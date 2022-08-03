17573a40e5143696e2fe0d59dbf75b78991e0e8b
panilya <panilyau@gmail.com>
2022-08-03 22:08:16 +0300

    Add '@JsonProperty on fields in order to correctly maps requests to Java Objects'

--- mochaccino-server/src/main/java/com/panilya/mochaccinoserver/model/RequestEntity.java
+++ mochaccino-server/src/main/java/com/panilya/mochaccinoserver/model/RequestEntity.java
@@ -1,14 +1,21 @@
 package com.panilya.mochaccinoserver.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class RequestEntity {
 
+    @JsonProperty("id")
     private final boolean id;
+    @JsonProperty("fullName")
     private final boolean fullName;
+    @JsonProperty("firstName")
     private final boolean firstName;
+    @JsonProperty("lastName")
     private final boolean lastName;
+    @JsonProperty("phoneNumber")
     private final boolean phoneNumber;
+    @JsonProperty("address")
     private final boolean address;
-
 
     //TODO: I guess there is a better way to implement this
     public RequestEntity(boolean fullName, boolean phoneNumber, boolean id, boolean firstName, boolean lastName, boolean address) {
@@ -18,5 +25,17 @@ public class RequestEntity {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
+    }
+    
+    @Override
+    public String toString() {
+        return "RequestEntity{" +
+                "id=" + id +
+                ", fullName=" + fullName +
+                ", firstName=" + firstName +
+                ", lastName=" + lastName +
+                ", phoneNumber=" + phoneNumber +
+                ", address=" + address +
+                '}';
     }
 }
