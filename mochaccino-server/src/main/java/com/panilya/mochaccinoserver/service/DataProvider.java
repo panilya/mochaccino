a8098896867de7016f627cdc42732b73bfd08c40
Ilya Pantsyr <panilyau@gmail.com>
2022-09-11 11:44:45 +0300

    Delete unnecessary class, fix imports

--- mochaccino-server/src/main/java/com/panilya/mochaccinoserver/service/DataProvider.java
+++ mochaccino-server/src/main/java/com/panilya/mochaccinoserver/service/DataProvider.java
@@ -4,7 +4,6 @@ import net.datafaker.Faker;
 
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
