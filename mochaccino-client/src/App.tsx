46f7518a091bcbbd26af4fe099e6988e1de1281f
Ilya Pantsyr <panilyau@gmail.com>
2022-09-23 22:30:25 +0300

    Add simple button and prototype of API endpoint in order to test code

--- mochaccino-client/src/App.tsx
+++ mochaccino-client/src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
 
 function App() {
+
+  const handleDownload = () => {
+      let body = {providers: ["firstName", "lastName", "zipCode"], "limit": 1000};
+      axios.post("https://mochaccino-server.herokuapp.com/data/download?format=csv", body,
+          {
+              headers: {
+                  "Content-Type": "application/json"
+              }
+          }).then((response) => {
+          console.log(response.headers);
+          const contentDispositionHeader = response.headers['content-disposition'];
+          const fileFormat = contentDispositionHeader.split('.')[1].slice(0, -1);
+          console.log(fileFormat);
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement('a');
+          link.href = url;
+          const desiredFileFormat = 'data.' + fileFormat;
+          link.setAttribute('download', desiredFileFormat);
+          document.body.appendChild(link);
+          link.click();
+      }).catch(err => {
+          console.log(err.status)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleDownload()}>
+        Download data!
+      </button>
     </div>
   );
 }
