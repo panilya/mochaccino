74e652fee54753aa587349f5023faa518c40b075
Ilya Pantsyr <panilyau@gmail.com>
2022-09-28 15:57:11 +0300

    Heroku deploy

--- mochaccino-server/src/main/java/com/panilya/mochaccinoserver/model/RequestEntity.java
+++ mochaccino-server/src/main/java/com/panilya/mochaccinoserver/model/RequestEntity.java
@@ -1,9 +1,12 @@
 package com.panilya.mochaccinoserver.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.beans.ConstructorProperties;
 import java.util.List;
 
 @Getter
@@ -19,6 +22,8 @@ public class RequestEntity {
     @JsonProperty("header")
     private final boolean header;
 
+    @JsonCreator
+    @ConstructorProperties({"providers", "limit", "separator", "header"})
     public RequestEntity(List<String> providers, int limit, String separator, boolean header) {
         this.providers = providers;
         this.limit = limit;
