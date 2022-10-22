7f93b00d6225844749f9331c5d6df4170818875f
Ilya Pantsyr <panilyau@gmail.com>
2022-10-22 23:28:02 +0300

    Add Swagger support

--- mochaccino-server/src/main/java/com/panilya/mochaccinoserver/model/GroupInfoEntity.java
+++ mochaccino-server/src/main/java/com/panilya/mochaccinoserver/model/GroupInfoEntity.java
@@ -1,12 +1,18 @@
 package com.panilya.mochaccinoserver.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.List;
 
 public class GroupInfoEntity {
 
+    @ApiModelProperty(name = "List of providers of the group", position = 1)
     private final List<ProviderInfo> providers;
+    @ApiModelProperty(name = "Name of the group", position = 2)
     private final String name;
+    @ApiModelProperty(name = "Description of the group")
     private final String description;
+    @ApiModelProperty(name = "ID of the group")
     private final Long id;
 
     public GroupInfoEntity(List<ProviderInfo> providers, String name, String description, Long id) {
